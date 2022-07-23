566234366947e485dfe8a6e108abc33bb0749389
Hema <hemasahu019@gmail.com>
2022-07-24 01:47:01 +0530

    [Hema Added UC3 Edit Existing Contacts using name]

--- Contact.cs
+++ Contact.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace AddressBookSystem
 {
     public class Contact
-    {   
+    {
         //Variables
         public string firstName;
         public string lastName;
@@ -17,11 +17,13 @@ namespace AddressBookSystem
         public int zipCode;
         public long phoneNumber;
         public string email;
+
+        //Method to add contact
         public static Contact AddContact()
         {
-            Contact contact = new Contact();
+            Contact contact = new Contact(); //created object to store user input
 
-            Console.WriteLine("Please enter the new contact details:");
+            Console.WriteLine("Please enter the contact details:");
             Console.Write("First Name: ");
             contact.firstName = Console.ReadLine();
             Console.Write("Last Name: ");
@@ -39,9 +41,56 @@ namespace AddressBookSystem
             Console.Write("Email: ");
             contact.email = Console.ReadLine();
 
-            return contact;
+            return contact; //returning object to store in list
 
         }
-    
+
+        //Method to diplay added contacts
+        public static void DisplayContact(List<Contact> addressBook)
+        {
+            if (addressBook.Count == 0) //if no contact found(empty list/Address Book)
+                Console.WriteLine("No Contact Added!!!");
+            else
+            {
+                for (int i = 0; i < addressBook.Count; i++)
+                {
+                    Console.WriteLine(addressBook[i]); 
+                }
+            }
+        
+        }
+
+        //Method to edit contact based on name
+        public static void EditContact(List<Contact> addressBook)
+        {
+            int contactFound = 0;
+
+            if (addressBook.Count == 0) //if no contact found(empty list/Address Book)
+                Console.WriteLine("No Contact Added!!!");
+            else
+            {
+                Console.WriteLine("Enter the Contact First Name ");
+                string newFirstName = Console.ReadLine();
+                Console.WriteLine("Enter the Contact First Name ");
+                string newLastName = Console.ReadLine();
+
+                for (int i = 0; i < addressBook.Count; i++)
+                {
+                    if (addressBook[i].firstName == newFirstName && addressBook[i].lastName == newLastName)
+                    {
+                        Console.WriteLine("Please input the new values for contact");
+                        addressBook[i] = Contact.AddContact();
+                        contactFound = 1;
+                    }
+                }
+                if (contactFound == 0)
+                    Console.WriteLine("Contact Information you entered not found. Please check added contacts and then try again!!!!");
+            }
+        }
+
+        public override string ToString() //overriding ToString method of Object class to diplay the objects
+        {
+            return "\nFirst Name : " + this.firstName + "\nLast Name : " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip Code: " + this.zipCode + "\nPhone Number: " + this.phoneNumber + "\nEmail: " + this.email;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,31 +1,49 @@
 ﻿namespace AddressBookSystem
 { /// <summary>
-  /// UC2 : Ability to add new Contact to the Address Book
+  /// UC3 : Ability to edit existing contact person using their name
   /// </summary>
     public class Program
     {   //An entrypoint of Console App
         static void Main(string[] args)
         {
-            List<Contact> addressBook = new List<Contact>(); //Creating list to store the contact of Contact Type Class
-            int option;
-            Console.WriteLine("Welcome to the Address book");
-
-            //Loop to continue adding contact until user wants to exit
-            do   
             {
-                Console.WriteLine("\nEnter from the below options:");
-                Console.WriteLine("1. Add new contact");
-                Console.WriteLine("2. Exit");
+                List<Contact> addressBook = new List<Contact>(); //created list to store contacts
+                int option;
+                Console.WriteLine("\nWelcome to the Address book");
+                do
+                {
 
-                option = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\nEnter from the below options:");
+                    Console.WriteLine("1. Add new contact");
+                    Console.WriteLine("2. Display added contacts");
+                    Console.WriteLine("3. Edit Existing Contact Using Name");
+                    Console.WriteLine("4. Exit");
 
-                if (option == 1)
-                    addressBook.Add(Contact.AddContact()); //Contact.AddContact() return object, storing object to addressBook
-                else
-                    Console.WriteLine("Enter valid option");
-                
+                    option = Convert.ToInt32(Console.ReadLine());
 
-            } while (option != 2); //continue till user wants to add contact
+                    //Loop to continue until user wants to exit
+                    switch (option)
+                    {
+                        case 1:
+                            addressBook.Add(Contact.AddContact());
+                            break;
+                        case 2:
+                            Contact.DisplayContact(addressBook);
+                            break;
+                        case 3:
+                            Contact.EditContact(addressBook);
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Input");
+                            break;
+                    }
+
+
+                } while (option != 4);
+
+            }
         }
     }
 }
