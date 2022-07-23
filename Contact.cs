7e2c16260616f8180cab71896d922db31d9852f3
Hema <hemasahu019@gmail.com>
2022-07-24 00:56:18 +0530

    [Hema Added UC1 Ability to create Contact]

---
+++ Contact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {   
+        //Variables
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zipCode;
+        public long phoneNumber;
+        public string email;
+
+        //Constuctor - Can initialize value while creating object
+        public Contact()
+        {
+            Console.WriteLine("Please enter the new contact details:");
+            Console.Write("First Name: ");
+            this.firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            this.lastName = Console.ReadLine();
+            Console.Write("Address: ");
+            this.address = Console.ReadLine();
+            Console.Write("City: ");
+            this.city = Console.ReadLine();
+            Console.Write("State: ");
+            this.state = Console.ReadLine();
+            Console.Write("Zip Code: ");
+            this.zipCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Phone Number: ");
+            this.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email: ");
+            this.email = Console.ReadLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿namespace AddressBookSystem
-{
+{ /// <summary>
+  /// UC1 : Ability to create Contact in Address Book
+  /// </summary>
     internal class Program
-    {
+    {   //An entrypoint of Console App
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Address Book Program");
+            Console.ReadLine();
+
+            Contact contact = new Contact(); //create contact by creating instance of Contact Class
+            Console.WriteLine(contact.email);
         }
     }
 }
